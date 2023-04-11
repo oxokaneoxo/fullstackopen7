7cf51cbba0eadc5c3c5ab334fd8d31a25db178a1
oxokaneoxo <kane.miller@hotmail.com>
2023-04-11 14:54:47 -0700

    7.1: routed anecdotes, step1

--- part7/routed-anecdotes-main/src/App.js
+++ part7/routed-anecdotes-main/src/App.js
@@ -1,4 +1,6 @@
+import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
+import { Routes, Route, Link, Navigate, useParams, useNavigate, useMatch } from "react-router-dom"
 
 const Menu = () => {
   const padding = {
@@ -6,9 +8,16 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link to="/">
+        <a href='/' style={padding}>anecdotes</a>
+      </Link>
+      <Link to="create">
+        <a href='create' style={padding}>create new</a>
+      </Link>
+      <Link to="about">
+        <a href='about' style={padding}>about</a>
+      </Link>
+
     </div>
   )
 }
@@ -74,7 +83,7 @@ const CreateNew = (props) => {
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
         </div>
         <button>create</button>
       </form>
@@ -126,9 +135,11 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </div>
   )
