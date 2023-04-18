90456ab0d1a72af9867966002c481e350c8eb96e
oxokaneoxo <kane.miller@hotmail.com>
2023-04-18 15:04:40 -0700

    8.10: Adding a book

--- part8/library-frontend-main/src/App.js
+++ part8/library-frontend-main/src/App.js
@@ -5,6 +5,7 @@ import NewBook from './components/NewBook'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [error, setError] = useState(null)
 
   return (
     <div>
@@ -13,12 +14,16 @@ const App = () => {
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
       </div>
+      
+      <div>
+        {error}
+      </div>
 
       <Authors show={page === 'authors'} />
 
       <Books show={page === 'books'} />
 
-      <NewBook show={page === 'add'} />
+      <NewBook setError={setError} show={page === 'add'} />
     </div>
   )
 }
--- part8/library-frontend-main/src/components/NewBook.js
+++ part8/library-frontend-main/src/components/NewBook.js
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+
+import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -6,15 +9,26 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+  
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS } ],
+    onError: (error) => {
+      const errors = error.graphQLErrors[0].extensions.error.errors
+      const messages = Object.values(errors).map(e => e.message).join('\n')
+      props.setError(messages)
+    }
+  })
 
   if (!props.show) {
     return null
   }
 
+
   const submit = async (event) => {
     event.preventDefault()
 
     console.log('add book...')
+    createBook({ variables: { title, author, published: Number(published), genres } })
 
     setTitle('')
     setPublished('')
